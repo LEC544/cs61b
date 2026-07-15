05c32d8a36b8671a05b611cbd88f47c68da7725e
joshua lee <liwanghao752@outlook.com>
2026-07-16 01:37:30 +0800

    rewrite equals

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -150,7 +150,7 @@ public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
         Deque<T> o = (Deque<T>) object;
         if (o.size() == this.size) {
             for (int i = 0; i < size; i += 1) {
-                if (this.get(i).equals(o.get(i))) {
+                if (! this.get(i).equals(o.get(i))) {
                     return false;
                 }
             }
